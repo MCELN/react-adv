f1fc2d2dc235d13d6f4a661c3913dea754539bae
Cristian Lanza <cristianlanza@gmail.com>
2023-09-08 21:29:40 -0300

    Control props alt

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,35 +1,29 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Product, onChangeArgs } from "../interfaces/interfaces";
 
 interface useProductArgs {
     product: Product;
-    onChange?: ( args: onChangeArgs ) => void;
+    onChange?: (args: onChangeArgs) => void;
     value?: number;
 }
 
-export const useProduct = ( { onChange, product, value = 0 }: useProductArgs ) => {
+export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => {
 
-    const [ counter, setCounter ] = useState( value );
+    const [counter, setCounter] = useState(value);
 
-    const isControlled = useRef( !!onChange );
+    const increaseBy = (value: number) => {
 
-    const increaseBy = ( value: number ) => {
-
-        if( isControlled.current ) {
-            return onChange!({ count: value, product })
-        }
-
-        const newValue = Math.max( counter + value, 0 );
-        setCounter( newValue );
+        const newValue = Math.max(counter + value, 0);
+        setCounter(newValue);
 
         onChange && onChange({ count: newValue, product });
     }
 
-    useEffect( () => {
-        setCounter( value );
-    }, [ value ])
+    useEffect(() => {
+        setCounter(value);
+    }, [value])
 
-    return {     
+    return {
         counter,
         increaseBy
     }
